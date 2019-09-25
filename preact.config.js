ce88518252b8b8c6ae1e763a9d8c411acfcd8f56
Patrick Klein <patrick.sean.klein@rwth-aachen.de>
2019-09-25 15:15:29 +0200

    Created env and mock api.

--- preact.config.js
+++ preact.config.js
@@ -1,11 +1,24 @@
+import Dotenv from 'dotenv-webpack'
+import apiMocker from 'connect-api-mocker'
+
 export default (config, env, helpers) => {
-  delete config.entry.polyfills;
-  config.output.filename = "[name].js";
+    delete config.entry.polyfills;
+    config.output.filename = "[name].js";
 
-  let { plugin } = helpers.getPluginsByName(config, "ExtractTextPlugin")[0];
-  plugin.options.disable = true;
+    let { plugin } = helpers.getPluginsByName(config, "ExtractTextPlugin")[0];
+    plugin.options.disable = true;
 
-  if (env.production) {
-    config.output.libraryTarget = "umd";
-  }
+    if (env.production) {
+        config.output.libraryTarget = "umd";
+    }
+
+    // Add support for .env files
+    config.plugins.push(new Dotenv({
+        path: env.production ? './production.end' : './.development.env',
+        defaults: true
+    }));
+
+    config.devServer.before = function (app) {
+        app.use(apiMocker('/api', 'api-mocks/'))
+    }
 };
