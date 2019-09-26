51c6f4a832c6f47188620c29adc6d8f021597be7
Karthik Narumanchi <karthiknr@gmail.com>
2019-09-26 15:11:14 +0200

    Add placeholder tests

--- src/components/stock-detail/index.test.js
+++ src/components/stock-detail/index.test.js
@@ -4,3 +4,8 @@ import { h } from "preact";
 import render from "preact-render-to-string";
 import StockDetail from "./index";
 
+describe("Placeholder Test", () => {
+    it("Test", () => {
+      expect(1 + 2).toEqual(3);
+    });
+  });
