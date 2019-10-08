5333157c3902a39d95ccecb44bf35acafc3eefa2
Patrick Klein <patrick.sean.klein@rwth-aachen.de>
2019-10-08 13:02:56 +0200

    npm run build now produces a single bundle which follows the WSO plugin architecture.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
 let poly = require("preact-cli/lib/lib/webpack/polyfills");
 
-import { h, render } from "preact";
-import Router from 'preact-router';
+import habitat from 'preact-habitat'
 import RecommendedStocksWidget from "./components/recommended-stocks";
+const { render } = habitat(RecommendedStocksWidget);
 
 const props = {
   "user": 12345678,
   "portfolio": 87654321
 };
 
-const Main = () => (
-	<Router>
-		<RecommendedStocksWidget user={props.user} portfolio={props.portfolio} path="/"/>
-	</Router>
-);
+function renderStockRecommendation(root_elem, user_id, portfolio_id) {
+    // Set properties
+    root_elem.dataset.propUser = props.user;
+    root_elem.dataset.propPortfolio = props.portfolio;
+	render({
+        selector: `#${root_elem.id}`
+	});
+}
 
-render(<Main />, document.body);
+registerWOPlugin("SecuritiesRecommendationEngine",
+    containerId => renderStockRecommendation(
+        document.getElementById(containerId),
+        WOPluginInfoProvider.currentUserId(),
+        WOPluginInfoProvider.currentPortfolioId()
+    )
+);
