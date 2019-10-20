59c104bf7df0383bec96b34d38d2e0ddb91eb891
Patrick Klein <patrick.sean.klein@rwth-aachen.de>
2019-10-20 19:32:57 +0200

    Implemented dialog-pane component.
    Renders it's content in a dialog.

---
+++ src/components/dialog-pane/index.test.js
@@ -0,0 +1,10 @@
+/* eslint-env node, jest */
+
+import render from "preact-render-to-string";
+import DialogPane from "./index";
+
+describe("Placeholder Test", () => {
+    it("Test", () => {
+      expect(1 + 2).toEqual(3);
+    });
+  });
